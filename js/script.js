f0af51c0a1797ef678035467acbca31540a62762
Ly Tuan Kiet <60308445+7heKnight@users.noreply.github.com>
2022-07-18 14:21:22 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.onload = function load_title() {
+    let path = window.location.pathname.split("/").pop()
+    if (path === "about.html")
+        document.getElementById("title").innerText="About me";
+    else if (path === "category.html")
+        document.getElementById("title").innerText="Category";
+    else if (path === "index.html")
+        document.getElementById("title").innerText="Home Page";
+    else
+        document.getElementById("title").innerText="Reading blog";
+}
+
+function show_table_category(){
+    document.getElementById('category_header').style.backgroundColor = "black";
+    document.getElementById('category_header').style.borderRadius = '10px';
+}
+
+function hide_table_category(){
+    document.getElementById('category_header').style.backgroundColor = "#353535";
+    document.getElementById('category_header').style.borderStyle = "none";
+}
